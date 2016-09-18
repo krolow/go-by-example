df1430d5cf3816f610d3072d9273a63d42c2b042
Vinícius Krolow <krolow@gmail.com>
2016-09-18 18:13:45 -0300

    Add ticker example

---
+++ tickers/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+	ticker := time.NewTimer(time.Millisecond * 500)
+	go func() {
+		for t := range ticker.C {
+			fmt.Println("Tick at", t)
+		}
+	}()
+
+	time.Sleep(time.Millisecond * 1600)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+}
